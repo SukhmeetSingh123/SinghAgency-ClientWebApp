57d6cd8032001dc48d5ef648d86edfc83897de2c
Sukhmeet <sukhmeetsingh1122002@gmail.com>
2024-08-04 16:53:50 +0530

    fixed redux error

--- Redux/provider.tsx
+++ Redux/provider.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { Provider } from "react-redux";
 import store from "./store";
+import { ReactNode } from "react";
 
-export function Providers({children}) {
-    return(
+interface ProvidersProps {
+    children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+    return (
         <Provider store={store}>
             {children}
         </Provider>
-    )
+    );
 }
